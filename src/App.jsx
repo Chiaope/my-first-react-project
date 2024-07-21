32f457602e2f398e8c17b1e522f06cbed30eddf7
gui kang <gui_kang@hotmail.com>
2024-07-21 14:23:55 +0800

    changed image to be loaded dynamically from import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import reactAtomImage from "./assets/react-core-concepts.png"
+
 function Header() {
   return <header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={reactAtomImage} alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
       Fundamental React concepts you will need for almost any app you are
