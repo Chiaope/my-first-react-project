a1b5e1a09f522c846090f657af74bd472d1f548c
gui kang <gui_kang@hotmail.com>
2024-07-27 22:48:54 +0800

    updated list item to use key and added mapping for the menu items

---
+++ src/components/CoreComponent/CoreConcept.jsx
@@ -0,0 +1,9 @@
+export default function CoreConcept({ title, description, image }) {
+    return (
+      <>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </>
+    )
+  }
